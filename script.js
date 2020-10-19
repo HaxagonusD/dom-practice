37680c9d42c4b2356f9941295b8d995f2e31377e
HaxagonusD <jquezad1@binghamton.edu>
2020-10-19 19:58:49 -0400

    Done

--- script.js
+++ script.js
@@ -20,9 +20,35 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  (()=>{
+    const something = document.getElementById("main-title")
+    something.innerText = "Welcome";
+    const body = document.querySelector('body')
+    body.style.backgroundColor = "red";
+    const list = document.getElementById("favorite-things")
+    list.removeChild(list.lastElementChild);
+    const specials = document.getElementsByClassName('special-title')
+    for (let i = 0; i < specials.length; i++) {
+      specials[i].style.fontSize = "2rem"
+    }
+    const past = document.getElementById("past-races")
+    past.removeChild(past.childNodes[2])
+    const london = document.createElement("li")
+    london.innerText = "San Fran"
+    past.appendChild(london)
+    const blog = document.createElement("div")
+    blog.className = "blog-post purple"
+    const title = document.createElement("h1")
+    title.innerText = "San Fran"
+    const text = document.createElement("p")
+    text.innerText = "Apparently in Cali"
+    blog.appendChild(title)
+    blog.appendChild(text)
+    const blogList = document.querySelector(".main")
+    blogList.insertBefore(blog, blogList.children[4]);
+  })()
   // Part 2
-
+ 
   // Part 3
 
   // Part 4
